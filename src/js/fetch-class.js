facfc77a72d3a837522cae55b7c3551f80f9b3bb
NataliaBulanova <nbulanova91@gmail.com>
2021-12-04 16:50:26 +0200

    update markup

--- src/js/fetch-class.js
+++ src/js/fetch-class.js
@@ -44,7 +44,7 @@ export class RenderClass {
     if (arrOfGenres.length >= 3) {
       arrOfGenres.splice(2, arrOfGenres.length - 2, 'Other');
     }
-    return arrOfGenres.join(',');
+    return arrOfGenres.join(', ');
   };
 
   renderHomePage = async () => {
@@ -61,15 +61,20 @@ export class RenderClass {
     const popularFilmsMarkup = films
       .map(({ original_title, poster_path, release_date, vote_average, genre_ids }) => {
         const data = new Date(release_date).getFullYear();
-        return `    <li class="film-list__item">
-    <div><img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="" width="300px" loading="lazy"/></div>
-    <div>
-      <h4 class="film-list__item-title">${original_title}</h4>
-      <p class="film-list__item-text">${this.replaceIdsWithGenres(
-        genre_ids,
-      )}${data}<span class="film-list__item-rating">${vote_average}</span></p>
-    </div>
-  </li>
+        return `   <li class="film-list__item list-item">
+        <a class="list-item__link" href="">
+        <div class="film-list__thumb">
+        <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="" width="300px" loading="lazy"/>
+      </div>
+      <div class="film-list__content">
+        <h3 class="film-list__item-title">${original_title}</h3>
+        <p class="film-list__item-text">${this.replaceIdsWithGenres(
+          genre_ids,
+        )} &VerticalLine; ${data}</p>
+      </div>
+</a>
+
+      </li>
 `;
       })
       .join('');

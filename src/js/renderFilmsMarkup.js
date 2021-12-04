30ac2ff98670a2d5ce7ba8a399aa32e8be75010c
vadger <vadger@yahoo.com>
2021-12-04 21:48:11 +0200

    added new fetch from form and refactor function fetchFilms, added poster without image

--- src/js/renderFilmsMarkup.js
+++ src/js/renderFilmsMarkup.js
@@ -3,26 +3,30 @@ import { getRefs } from './getRefs';
 const refs = getRefs();
 export const renderFilmsMarkup = films => {
   const popularFilmsMarkup = films
-    .map(({ original_title, poster_path, release_date, genre_ids }) => {
+    .map(({ title, poster_path, release_date, genre_ids }) => {
+      let imageSrc = 'default-image.jpg';
+        if(poster_path) {
+          imageSrc = `https://image.tmdb.org/t/p/w500${poster_path}`;
+        }
       const data = new Date(release_date).getFullYear();
       return `<li class="film-list__item list-item">
         <a class="list-item__link" href="">
           <div class="film-list__thumb">
             <img
-              src="https://image.tmdb.org/t/p/w500${poster_path}"
-              alt=""
+              src="${imageSrc}"
+              alt="${title}"
               width="300px"
               loading="lazy"
             />
           </div>
           <div class="film-list__content">
-            <h3 class="film-list__item-title">${original_title}</h3>
+            <h3 class="film-list__item-title">${title}</h3>
             <p class="film-list__item-text">
               ${convertIdsToGenres(genre_ids)} &VerticalLine; ${data}
             </p>
           </div>
         </a>
-      </li>      
+      </li>
 `;
     })
     .join('');

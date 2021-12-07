29dac072add7e0206d3d9237a5e43884a9b0bc2f
Althaen <Evgenius16.k@gmail.com>
2021-12-08 00:36:59 +0300

    Правит ссылки в хедере и добавляет слушателей события на кнопки в библиотеке пользователя

--- src/js/main.js
+++ src/js/main.js
@@ -39,3 +39,17 @@ refs.container.addEventListener('click', e => {
 
 refs.myLibrary.addEventListener('click', onMyLibraryClick);
 refs.homeLink.addEventListener('click', onHomeClick);
+refs.showWatchedBtn.addEventListener('click', e => {
+  if (!refs.showWatchedBtn.classList.contains('header__button--active')) {
+    refs.showWatchedBtn.classList.add('header__button--active');
+    refs.showQueueBtn.classList.remove('header__button--active');
+    // Серёга, добавляй свою функцию после этой строчки :)
+  }
+});
+refs.showQueueBtn.addEventListener('click', e => {
+  if (refs.showWatchedBtn.classList.contains('header__button--active')) {
+    refs.showWatchedBtn.classList.remove('header__button--active');
+    refs.showQueueBtn.classList.add('header__button--active');
+    // Серёга, добавляй свою функцию после этой строчки :)
+  }
+});

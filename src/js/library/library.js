962f60e487a59c8fd631f2053dc3ff532c4311a4
NataliaBulanova <nbulanova91@gmail.com>
2021-12-10 19:03:02 +0200

    on myLibrary click render

--- src/js/library/library.js
+++ src/js/library/library.js
@@ -1,12 +1,14 @@
 import { refs } from '../refs';
 import { onLibraryBtnClick } from './onLibraryBtnClick';
 
+refs.myLibrary.addEventListener('click', () => {
+  onLibraryBtnClick('watchedStorage');
+});
+
 refs.showWatchedBtn.addEventListener('click', () => {
   onLibraryBtnClick('watchedStorage');
-  console.log('im here');
 });
 
 refs.showQueueBtn.addEventListener('click', () => {
   onLibraryBtnClick('savedStorage');
-  console.log('im here');
 });

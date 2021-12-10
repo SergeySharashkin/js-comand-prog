5054c0d2d2f5a6333d3a1c6da1c8cafad644012f
NataliaBulanova <nbulanova91@gmail.com>
2021-12-10 10:26:23 +0200

    lost import

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ import { onMyLibraryClick, onHomeClick } from './header';
 import { pagination } from './pagination';
 import { renderFilmsMarkup } from './renderFilmsMarkup';
 import './Trailer/onTrailerBtnClick';
+import './library/library';
 
 // import './snow';
 

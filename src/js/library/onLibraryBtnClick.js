b629d34c95d756d77c9ba7df34160e5ce71bd47d
vadger <vadger@yahoo.com>
2021-12-11 15:47:20 +0200

    fixes for pagination on library

--- src/js/library/onLibraryBtnClick.js
+++ src/js/library/onLibraryBtnClick.js
@@ -1,7 +1,20 @@
 import { renderLibraryMarkup } from './renderLibraryMarkup';
-import { getStorageItems, inLibraryFilms } from './getStorageItems';
+import { getByPage, getStorageItems, inLibraryFilms } from './getStorageItems';
+import {pagination} from '../pagination';
 
 export function onLibraryBtnClick(librarySection) {
   getStorageItems(librarySection);
-  renderLibraryMarkup(inLibraryFilms);
+  buildPagination();
+}
+
+function onPaginationClick({page}) {
+  localStorage.setItem('library', page);
+  buildPagination();
+}
+
+function buildPagination()
+{
+  const page = localStorage.getItem('library') || 1;
+  renderLibraryMarkup(getByPage(page));
+  pagination({totalItems: inLibraryFilms.length, page}, onPaginationClick);
 }

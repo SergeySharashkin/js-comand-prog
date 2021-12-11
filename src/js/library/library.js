110ed6ade48e90e703fda688424a80388e689ea9
NataliaBulanova <nbulanova91@gmail.com>
2021-12-11 23:19:52 +0200

    add something to sessionStorage, even don't know is it right. Have no clue

--- src/js/library/library.js
+++ src/js/library/library.js
@@ -4,12 +4,18 @@ import { pagination } from '../pagination';
 
 refs.myLibrary.addEventListener('click', () => {
   onLibraryBtnClick('watchedStorage');
+  sessionStorage.clear();
+  sessionStorage.setItem('watchedPage', '1');
 });
 
 refs.showWatchedBtn.addEventListener('click', () => {
   onLibraryBtnClick('watchedStorage');
+  sessionStorage.clear();
+  sessionStorage.setItem('watchedPage', '1');
 });
 
 refs.showQueueBtn.addEventListener('click', () => {
   onLibraryBtnClick('savedStorage');
+  sessionStorage.clear();
+  sessionStorage.setItem('savedPage', '1');
 });

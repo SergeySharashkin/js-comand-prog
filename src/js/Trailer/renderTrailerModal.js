fafe7585ca6d9643bd253c7e49d5f76e1b3e7248
NataliaBulanova <nbulanova91@gmail.com>
2021-12-12 13:22:10 +0200

    reject autoplay

--- src/js/Trailer/renderTrailerModal.js
+++ src/js/Trailer/renderTrailerModal.js
@@ -2,7 +2,7 @@ import { refs } from '../refs';
 
 export function renderTrailerModal(key) {
   refs.trailerThumb.style.backgroundImage = `url(https://img.youtube.com/vi/${key}/maxresdefault.jpg)`;
-  const trailerUrl = `https://www.youtube.com/embed/${key}?autoplay=1&enablejsapi=1&rel=0&modestbranding=1&`;
+  const trailerUrl = `https://www.youtube.com/embed/${key}?autoplay=0&enablejsapi=1&rel=0&modestbranding=1&`;
   const video = `<iframe
       id="player"
       src="${trailerUrl}"

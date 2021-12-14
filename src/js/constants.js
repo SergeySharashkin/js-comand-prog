26f0cb8e01f42c3c293cc374c339a26f576a1628
NataliaBulanova <nbulanova91@gmail.com>
2021-12-14 02:57:59 +0200

    complete multilanguage, add state saving

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,8 +3,17 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const POPULAR_FILM_FETCH = '/trending/movie/week';
 const RELEVANT_GENRES_LIST = '/genre/movie/list';
 const SEARCH__MOVIE = '/search/movie';
+const SEARCH_BY_ID = '/movie/';
 let LANGUAGE = JSON.parse(localStorage.getItem('language'));
 if (!LANGUAGE) {
   LANGUAGE = 'ru';
 }
-export { KEY, BASE_URL, POPULAR_FILM_FETCH, RELEVANT_GENRES_LIST, SEARCH__MOVIE, LANGUAGE };
+export {
+  KEY,
+  BASE_URL,
+  POPULAR_FILM_FETCH,
+  RELEVANT_GENRES_LIST,
+  SEARCH__MOVIE,
+  LANGUAGE,
+  SEARCH_BY_ID,
+};
--- src/js/library/renderLibraryMarkup.js
+++ src/js/library/renderLibraryMarkup.js
@@ -1,47 +1,72 @@
 import coverImage from '../../images/default_image.jpg';
 import { refs } from '../refs';
+import { convertIdsToGenres } from '../getGenres';
+import axios from 'axios';
+import { KEY, BASE_URL, LANGUAGE, SEARCH_BY_ID } from '../constants';
 
 export const renderLibraryMarkup = films => {
-  const filmsMarkup = films
-    .map(({ alt, date, count, genres, id, original, overview, popularity, rating, url }) => {
-      let imageSrc = coverImage;
-      if (url) {
-        imageSrc = url;
-      }
-      if (!original) {
-        original = '---';
-      }
-      return `<li class="film-list__item list-item">
-          <a class="list-item__link" href="#" >
-            <div class="list-item__thumb">
-              <img
+  if (!films.length) {
+    refs.container.innerHTML = '';
+    return;
+  }
+  films.map(({ id }) => {
+    refs.container.innerHTML = '';
+    axios
+      .get(`${BASE_URL}${SEARCH_BY_ID}${id}?api_key=${KEY}&language=${LANGUAGE}`)
+      .then(response => response.data)
+      .then(
+        ({
+          genres,
+          original_title,
+          overview,
+          popularity,
+          poster_path,
+          release_date,
+          title,
+          vote_average,
+          vote_count,
+        }) => {
+          let imageSrc = coverImage;
+          if (poster_path) {
+            imageSrc = `https://image.tmdb.org/t/p/w500${poster_path}`;
+          }
+          if (!original_title) {
+            original_title = '---';
+          }
+          const date = new Date(release_date).getFullYear();
+          const libraryMarkup = `<li class="film-list__item list-item">
+              <a class="list-item__link" href="#" >
+                <div class="list-item__thumb">
+                <img
                 class="list-item__img"
                 src="${imageSrc}"
-                alt="${alt}"
+                alt="${title}"
                 data-id="${id}"
                 data-url="${imageSrc}"
-                data-original="${original}"
+                data-original="${original_title}"
                 data-overview="${overview}"
                 data-popularity="${popularity}"
-                data-rating="${rating}"
-                data-count="${count}"
-                data-genres="${genres}"
+                data-rating="${vote_average}"
+                data-count="${vote_count}"
+                data-genres="${convertIdsToGenres(genres)}"
+                data-date="${date}"
                 width="300px"
                 loading="lazy"
               />
-            </div>
-            <div class="list-item__content">
-              <h3 class="list-item__title">${alt}</h3>
-              <div class="list-item__info">
-                <p class="list-item__text">${genres} &VerticalLine; ${date}
-                  &nbsp<span class="rating-span">${rating}</span>
-                </p>
-              </div>
-            </div>
-          </a>
-        </li>
-  `;
-    })
-    .join('');
-  refs.container.innerHTML = filmsMarkup;
+                  </div>
+                <div class="list-item__content">
+                  <h3 class="list-item__title">${title}</h3>
+                  <div class="list-item__info">
+                    <p class="list-item__text">${convertIdsToGenres(genres)} &VerticalLine; ${date}
+                      &nbsp<span class="rating-span">${vote_average}</span>
+                    </p>
+                  </div>
+                </div>
+              </a>
+            </li>
+      `;
+          return refs.container.insertAdjacentHTML('beforeend', libraryMarkup);
+        },
+      );
+  });
 };

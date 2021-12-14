a511b9d496eb496f6c1e65daf3e6f4e5f2da3352
AlexNumo <korol1511@gmail.com>
2021-12-14 15:30:52 +0200

    3-d modal

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -1,18 +1,18 @@
-// // import SimpleLightbox from 'simplelightbox';
-// // import 'simplelightbox/dist/simple-lightbox.min.css';
+// // // import SimpleLightbox from 'simplelightbox';
+// // // import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// const photo = document.querySelector(".footer__link");
+// // const photo = document.querySelector(".footer__link");
 
-// photo.addEventListener('click', checkModal);
+// // photo.addEventListener('click', checkModal);
 
-// function checkModal() { 
-//   const modal = document.querySelector("about-us");
-//   modal.classList.add(".about-us__visible");
-//   if (modal.classList.contains(".about-us__visible")) {
-//     modal.classList.remove(".about-us__visible");
-//    }
+// // function checkModal() { 
+// //   const modal = document.querySelector("about-us");
+// //   modal.classList.add(".about-us__visible");
+// //   if (modal.classList.contains(".about-us__visible")) {
+// //     modal.classList.remove(".about-us__visible");
+// //    }
 
-// }
+// // }
 
 // Get the modal
 var modal = document.getElementById("myModal");
@@ -40,11 +40,28 @@ window.onclick = function(event) {
   }
 }
 
-const alexNumo = document.querySelector(".alex-numo");
+const alexNumo = document.querySelector(".sergyi");
 console.log(alexNumo);
-alexNumo.addEventListener('click', aboutAlexNumo);
+alexNumo.addEventListener('onclick', aboutSergyi);
+const findChange = document.querySelector(".modal-header");
+console.log(findChange);
 
-function aboutAlexNumo() {
+findChange.insertAdjacentHTML('beforeend', `<div class="modal-body">
+    <a class='btn btn-primary' href="#" style="pointer-events: none"><img class="member-photo" src="./images/member_logo.jpg" alt="AlexNumo" /></a>
+    <div class="member-info-position">
+        <h3>AlexNumo</h3>
+        <ul class="info-about-member-work">
+            <li class="about-us-marker">
+                <p>ЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕЕ</p>
+            </li>
+            <li class="about-us-marker">
+                <p>ЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫЫ</p>
+            </li>
+        </ul>
+    </div>
+</div>`);
+
+function aboutSergyi() {
   `<div class="modal-body">
     <a class='btn btn-primary' href="#" style="pointer-events: none"><img class="member-photo" src="./images/member_logo.jpg" alt="AlexNumo" /></a>
     <div class="member-info-position">
@@ -60,3 +77,40 @@ function aboutAlexNumo() {
     </div>
 </div>`
 }
+function aboutAlexNumo() {
+  `<div class="modal-body">
+    <a class='btn btn-primary' href="#"><img class="member-photo" src="./images/member_logo.jpg" alt="AlexNumo" /></a>
+    <div class="member-info-position">
+      <h3>AlexNumo</h3>
+      <ul class="info-about-member-work">
+        <li class="about-us-marker">
+          <p>Падающий снег</p>
+        </li>
+        <li class="about-us-marker">
+          <p>Работа нашей команды</p>
+        </li>
+      </ul>
+    </div>
+  </div>`
+}
+
+// import SimpleLightbox from "simplelightbox";
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// // let simple = document.getElementById("myBtn").onclick = lightboxSM; return false
+
+// // function lightboxSM() {
+// //   console.log("SASAMBA");
+// // }
+
+
+// var lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'title',
+//   captionDelay: 250,
+//   close: true,
+//   widthRatio: 0.5,
+//   heightRatio: 0.5,
+//   maxZoom: 0,
+//   focus: false,
+//   disableScroll: true,
+// });

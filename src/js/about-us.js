a3ac5f8eee449d16148e877d45142e705316379b
AlexNumo <korol1511@gmail.com>
2021-12-16 16:24:46 +0200

    Изменил анимацию на опасити, а так же поубирал лишние коменты кода

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -13,13 +13,11 @@
     },
   });
 
-const swiperActiv = document.querySelector(".backdrop-swiper");
 const footerLink = document.querySelector(".footer__link");
+const swiperActiv = document.querySelector(".backdrop-swiper");
 const swiperFix = document.getElementById('swiper-fix');
 const swiperClose = document.querySelector(".js-swiper-close");
 const swiperPosition = document.querySelector(".swiper-position");
-// const swiperCloseModal = document.querySelector(".backdrop-swiper");
-
 
 footerLink.addEventListener('click', ()=>{
   swiperActiv.style.display = 'block';
@@ -32,27 +30,16 @@ swiperClose.addEventListener('click', ()=>{
   
 });
 
+function handleModalClick(evt) {
+if (!evt.target.closest(".swiper-position")) {
+    swiperActiv.style.display = 'none';
+    swiperFix.classList.remove('swiper-fix');
+    }
+  }
+
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape') {
   swiperActiv.style.display = 'none';
   swiperFix.classList.remove('swiper-fix');
   }
 });
-
-// swiperActiv.addEventListener('click', onModalCloseSwiper);
-
-// function onModalCloseSwiper() {
-//   if (swiperPosition.addEventListener('click', () => {
-//     swiperActiv.style.display = 'block';
-//     swiperFix.classList.add("swiper-fix");
-//   })) { }
-//  swiperActiv.style.display = 'none';
-//   swiperFix.classList.remove('swiper-fix');
-// }
-
-
-// document.onclick = function(e){
-//     if ( e.target.className != 'swiper-position' && e.target.className != 'footer__link' ) {
-//         swiperActiv.style.display = 'none';
-//     };
-// };
